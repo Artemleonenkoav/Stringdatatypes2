b9ca001a76c310c8c375463d64b656456a9278b9
Artemleonenkoav <leonenkoavfin@gmail.com>
2021-08-03 13:13:14 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String text = "Вот мысль, которой весь я предан,\n"+
+                "Итог всего, что ум скопил.\n"+
+                "Лишь тот, кем бой за жизнь изведан,\n"+
+                "Жизнь и свободу заслужил.";
+        System.out.println(text);
+    }
+}
